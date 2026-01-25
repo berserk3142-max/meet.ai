6e338a343a29ebae5dd0563dec15f561b9bd4b1c
John Doe <117672208+3142akay@users.noreply.github.com>
2026-01-25 14:04:47 +0530

    Initial Next.js + shadcn setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold text-black dark:text-white">
+          Hello Aman 🚀
+        </h1>
+      </main>
+    </div>
+  );
+}
